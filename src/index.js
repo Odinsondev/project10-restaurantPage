22dc9554ab49f3c95db9ef3f599870f14d4bb257
Odinsondev <122601475+Odinsondev@users.noreply.github.com>
2024-04-11 18:28:29 -0700

    Feat: begin work on menu.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import './normalize.css';
 import './style.css';
 
 import renderHome from './home';
+import renderMenu from './menu';
 
 console.log("index test");
 
-renderHome();
+/* renderMenu(); */
